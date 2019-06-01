628284f13358eb0953a8f7b396840bad9a4cef73
kain <zhangtqx@vip.qq.com>
2019-06-01 18:40:02 +0800

    Submit

--- src/controller/main.controller.ts
+++ src/controller/main.controller.ts
@@ -1,18 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '../service/config.service';
-import { RedisService } from '../service/redis.service';
 
 @Controller('main')
 export class MainController {
-  constructor(
-    private configService: ConfigService,
-    private redisService: RedisService,
-  ) {
-  }
 
   @Get()
   index(): any {
-    this.redisService.client.set('test', 1);
     return [];
   }
 }
