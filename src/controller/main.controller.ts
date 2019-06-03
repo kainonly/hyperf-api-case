dc89c84f18c3c5f5a5a5eb6bec6079fd78abed44
kain <zhangtqx@vip.qq.com>
2019-06-03 17:25:48 +0800

    submit

--- src/controller/main.controller.ts
+++ src/controller/main.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('main')
 export class MainController {
   @Get()
   index(): any {
     return [];
+  }
+
+  @Post('verify')
+  verify(): any {
+    return {
+      error: 0,
+    };
+  }
+
+  @Post('menu')
+  menu(): any {
+    return {
+      error: 0,
+      data: [],
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ NestFactory.create<NestFastifyApplication>(
   new FastifyAdapter(),
   {
     cors: {
+      origin: ['http://localhost:4200'],
       credentials: true,
     },
   },
