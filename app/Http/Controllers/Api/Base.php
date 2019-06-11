895ac36f19128d8640343323f8d6b4351b7722bb
kain <zhangtqx@vip.qq.com>
2019-06-11 09:07:04 +0800

    always reset

--- app/Http/Controllers/Api/Base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\Api;
-
-use Laravel\Lumen\Routing\Controller as BaseController;
-
-class Base extends BaseController
-{
-}
--- app/Http/Controllers/System/Base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers\System;
-
-use Laravel\Lumen\Routing\Controller as BaseController;
-
-class Base extends BaseController
-{
-}
