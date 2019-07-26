295e7937f11109e2810768b92fb6a3961f3cfa82
kain <zhangtqx@vip.qq.com>
2019-07-26 16:52:25 +0800

    Update Example

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DbService } from './common/db.service';
-
 import { RouterEntity } from './database/router.entity';
-
 import { MainController } from './controller/main.controller';
 
 @Module({
