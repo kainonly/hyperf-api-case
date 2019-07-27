536ebe6139b7388556b5613c0aaeed6ba6fa91b6
kain <zhangtqx@vip.qq.com>
2019-07-28 00:10:08 +0800

    Set Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from './database/db.module';
 import { DbService } from './facade/db.service';
+import { RedisService } from './facade/redis.service';
+
 import { Main } from './api/main';
+import { ConfigService } from './facade/config.service';
 
 @Module({
-  controllers: [
-    Main,
-  ],
   imports: [
     DbModule,
   ],
+  controllers: [
+    Main,
+  ],
   providers: [
+    ConfigService,
     DbService,
+    RedisService,
   ],
 })
 export class AppModule {
