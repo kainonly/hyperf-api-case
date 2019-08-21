a08990546de9f11d8342f3a0ac38be377f4131b0
kain <zhangtqx@vip.qq.com>
2019-08-21 22:30:59 +0800

    Submit

--- app/Http/Api/Controllers/Index.php
+++ app/Http/Api/Controllers/Index.php
@@ -2,24 +2,10 @@
 
 namespace App\Http\Api\Controllers;
 
-use Illuminate\Support\Facades\DB;
-
 class Index extends Base
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function index()
     {
-        $data = DB::table('role')->get();
-        dump($data);
         return [
             'status' => 'ok'
         ];
