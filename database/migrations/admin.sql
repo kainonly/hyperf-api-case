289e5208843a2d4cba74e40971376b484d049cc8
kain <zhangtqx@vip.qq.com>
2019-08-31 09:53:08 +0800

    Update Migration

--- database/migrations/admin.sql
+++ database/migrations/admin.sql
@@ -10,7 +10,10 @@ select `v_admin_basic`.`id`                                           AS `id`,
        `v_admin_basic`.`call`                                         AS `call`,
        `v_admin_basic`.`status`                                       AS `status`,
        `v_admin_basic`.`create_time`                                  AS `create_time`,
-       `v_admin_basic`.`update_time`                                  AS `update_time`
+       `v_admin_basic`.`update_time`                                  AS `update_time`,
+       `v_admin_basic`.`email`                                        AS `email`,
+       `v_admin_basic`.`phone`                                        AS `phone`,
+       `v_admin_basic`.`avatar`                                       AS `avatar`
 from (`v_admin_basic`
          join `v_admin_role` on (`v_admin_role`.`admin_id` = `v_admin_basic`.`id`))
 group by `v_admin_basic`.`id`, `v_admin_basic`.`username`, `v_admin_basic`.`password`, `v_admin_basic`.`call`,
