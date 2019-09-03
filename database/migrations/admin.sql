2cc7d22df06dc75486d3da1146c906424e729440
kain <zhangtqx@vip.qq.com>
2019-09-03 11:27:33 +0800

    Fix Admin View

--- database/migrations/admin.sql
+++ database/migrations/admin.sql
@@ -8,13 +8,14 @@ select `v_admin_basic`.`id`                                           AS `id`,
        `v_admin_basic`.`password`                                     AS `password`,
        group_concat(distinct `v_admin_role`.`role_key` separator ',') AS `role`,
        `v_admin_basic`.`call`                                         AS `call`,
-       `v_admin_basic`.`status`                                       AS `status`,
-       `v_admin_basic`.`create_time`                                  AS `create_time`,
-       `v_admin_basic`.`update_time`                                  AS `update_time`,
        `v_admin_basic`.`email`                                        AS `email`,
        `v_admin_basic`.`phone`                                        AS `phone`,
-       `v_admin_basic`.`avatar`                                       AS `avatar`
+       `v_admin_basic`.`avatar`                                       AS `avatar`,
+       `v_admin_basic`.`status`                                       AS `status`,
+       `v_admin_basic`.`create_time`                                  AS `create_time`,
+       `v_admin_basic`.`update_time`                                  AS `update_time`
 from (`v_admin_basic`
          join `v_admin_role` on (`v_admin_role`.`admin_id` = `v_admin_basic`.`id`))
 group by `v_admin_basic`.`id`, `v_admin_basic`.`username`, `v_admin_basic`.`password`, `v_admin_basic`.`call`,
-         `v_admin_basic`.`status`, `v_admin_basic`.`create_time`, `v_admin_basic`.`update_time`;
+         `v_admin_basic`.`email`, `v_admin_basic`.`phone`, `v_admin_basic`.`avatar`, `v_admin_basic`.`status`,
+         `v_admin_basic`.`create_time`, `v_admin_basic`.`update_time`;
