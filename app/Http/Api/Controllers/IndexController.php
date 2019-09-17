949dc19d6437bc56b3adbb71a80aef54139717fa
kain <zhangtqx@vip.qq.com>
2019-09-17 17:57:12 +0800

    back

--- app/Http/Api/Controllers/IndexController.php
+++ app/Http/Api/Controllers/IndexController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Api\Controllers;
 
-use Illuminate\Support\Facades\DB;
-
 class IndexController extends BaseController
 {
     public function index()
     {
-        $data = DB::table('resource')->get();
         return [
             'status' => 'ok',
-            'resource' => $data->toArray()
         ];
     }
 }
