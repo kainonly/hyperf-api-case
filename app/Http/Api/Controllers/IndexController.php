c4f098b70a3671e71c1b59e38859ec44c8dfc79f
kain <zhangtqx@vip.qq.com>
2019-09-20 15:21:58 +0800

    Update

--- app/Http/Api/Controllers/IndexController.php
+++ app/Http/Api/Controllers/IndexController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Api\Controllers;
 
-
-use lumen\extra\facade\Cookie;
-
 class IndexController extends BaseController
 {
     public function index()
     {
-        Cookie::set('name', 'kain');
-        return [
-        ];
+        return [];
     }
 }
