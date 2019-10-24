43395208cdd005c8d094b5d6d049823f43424b52
kain <zhangtqx@vip.qq.com>
2019-10-24 18:05:18 +0800

    Submit

--- config/autoload/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'allow_origin' => explode(',', env('cors')),
-    'allow_credentials' => true,
-    'allow_methods' => ['POST'],
-    'expose_headers' => [],
-    'allow_headers' => ['CONTENT-TYPE', 'X-REQUESTED-WITH'],
-    'max_age' => 31536000,
-];
