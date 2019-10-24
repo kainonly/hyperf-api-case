47c6b535821ac67598af3585e2a9ce023d612408
kain <zhangtqx@vip.qq.com>
2019-10-24 18:26:25 +0800

    Update Acl Controller

--- app/Controller/Acl.php
+++ app/Controller/Acl.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use Hyperf\Curd\Common\AddModel;
+use Hyperf\Curd\Common\DeleteModel;
+use Hyperf\Curd\Common\EditModel;
+use Hyperf\Curd\Common\GetModel;
+use Hyperf\Curd\Common\ListsModel;
 use Hyperf\Curd\Common\OriginListsModel;
+use Hyperf\Curd\Lifecycle\AddAfterHooks;
+use Hyperf\Curd\Lifecycle\DeleteAfterHooks;
+use Hyperf\Curd\Lifecycle\EditAfterHooks;
+use Hyperf\DbConnection\Db;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\Middleware;
 use Hyperf\HttpServer\Annotation\Middlewares;
@@ -15,8 +24,59 @@ use Hyperf\HttpServer\Annotation\Middlewares;
  *  @Middleware(\App\Middleware\AppAuthVerify::class)
  * })
  */
-class Acl extends Base
+class Acl extends Base implements AddAfterHooks, EditAfterHooks, DeleteAfterHooks
 {
-    use OriginListsModel;
+    use OriginListsModel, ListsModel, AddModel, GetModel, EditModel, DeleteModel;
     protected $model = 'acl';
+
+    /**
+     * Add post processing
+     * @param string|int $id
+     * @return mixed
+     */
+    public function __addAfterHooks($id)
+    {
+        return true;
+    }
+
+    /**
+     * Modify post processing
+     * @return mixed
+     */
+    public function __editAfterHooks()
+    {
+        return true;
+    }
+
+    /**
+     * Delete post processing
+     * @return mixed
+     */
+    public function __deleteAfterHooks()
+    {
+        return true;
+    }
+
+    /**
+     * Exists Acl Key
+     * @return array
+     */
+    public function validedKey()
+    {
+        if (empty($this->post['key'])) {
+            return [
+                'error' => 1,
+                'msg' => 'error:require_key'
+            ];
+        }
+
+        $result = Db::table($this->model)
+            ->where('key', '=', $this->post['key'])
+            ->exists();
+
+        return [
+            'error' => 0,
+            'data' => $result
+        ];
+    }
 }
