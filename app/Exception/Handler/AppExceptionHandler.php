0b6f1e500536e5ec19ba57d4f7f2a1d13d90e797
kain <zhangtqx@vip.qq.com>
2019-10-25 17:12:12 +0800

    Update Exception

--- app/Exception/Handler/AppExceptionHandler.php
+++ app/Exception/Handler/AppExceptionHandler.php
@@ -32,9 +32,11 @@ class AppExceptionHandler extends ExceptionHandler
 
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
-        $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
-        $this->logger->error($throwable->getTraceAsString());
-        return $response->withStatus(500)->withBody(new SwooleStream('Internal Server Error.'));
+//        $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
+//        $this->logger->error($throwable->getTraceAsString());
+        return $response->withStatus(500)->withBody(new SwooleStream(
+                $throwable->getTraceAsString())
+        );
     }
 
     public function isValid(Throwable $throwable): bool
