0f01fc6264268d4728bffd6092d0a7c7c6f5978f
kain <zhangtqx@vip.qq.com>
2019-10-28 11:10:54 +0800

    Submit

--- app/Http/Api/Controllers/IndexController.php
+++ app/Http/Api/Controllers/IndexController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Api\Controllers;
 
-use Illuminate\Support\Facades\Request;
-
 class IndexController extends BaseController
 {
     public function index()
     {
         return [
-            'status' => 'ok',
+            'version' => 1.0,
         ];
     }
 }
