b98008770074411738cd8374894c75b769e0ddf0
kain <zhangtqx@vip.qq.com>
2019-10-29 12:39:46 +0800

    Submit

--- app/Http/Api/Controllers/IndexController.php
+++ app/Http/Api/Controllers/IndexController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Api\Controllers;
 
-use Illuminate\Support\Facades\Redis;
-
 class IndexController extends BaseController
 {
     public function index()
     {
-        Redis::set('name', 'kain');
         return [
             'version' => 1.0,
         ];
