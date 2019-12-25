5f8ecee9b62dca972aa3eb5bcfb793249e10e15b
kain <zhangtqx@vip.qq.com>
2019-12-25 22:03:23 +0800

    Update Van-Core submodule

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import * as fastify from 'fastify';
+import { Application } from 'van-core';
 
-const server: fastify.FastifyInstance = fastify({
+const app = new Application();
+
+app.bootstrap('127.0.0.1', 3000, {
   logger: true,
-});
-server.listen(3000, '127.0.0.1', (err, address) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-  server.log.info(`server listening on ${address}`);
+}).then(address => {
+  app.getServer().log.info(`server listening on ${address}`);
+}).catch(err => {
+  app.getServer().log.error(err);
+  process.exit(1);
 });
