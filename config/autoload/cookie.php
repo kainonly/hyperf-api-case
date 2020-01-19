9512e766963c7cbf4ea640f22681cd13f98deb21
kain <zhangtqx@vip.qq.com>
2020-01-19 09:31:33 +0800

    调试

--- config/autoload/cookie.php
+++ config/autoload/cookie.php
@@ -3,12 +3,12 @@
 // | Cookie设置
 // +----------------------------------------------------------------------
 return [
-    'expire' => 0,
-    'path' => '/',
-    'domain' => '',
-    'secure' => false,
-    'httpOnly' => false,
-    'setcookie' => true,
-    'raw' => '',
-    'samesite' => '',
+    'expire' => env('COOKIE_EXPIRE', 0),
+    'path' => env('COOKIE_PATH', '/'),
+    'domain' => env('COOKIE_DOMAIN', ''),
+    'secure' => env('COOKIE_SECURE', false),
+    'httponly' => env('COOKIE_HTTPONLY', false),
+    'setcookie' => false,
+    'raw' => false,
+    'samesite' => env('COOKIE_SAMESITE', null),
 ];
