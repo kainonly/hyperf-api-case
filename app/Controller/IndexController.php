265aa7d0dbaf4b349018cee09a9451879ccbf3f9
kain <zhangtqx@vip.qq.com>
2020-01-21 15:18:18 +0800

    测试Samesite

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -3,12 +3,25 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\HttpMessage\Cookie\Cookie;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+
 class IndexController
 {
-    public function index(): array
+    public function index(ResponseInterface $response)
     {
-        return [
+        return $response->withCookie(new Cookie(
+            'name',
+            'kain',
+            0,
+            '/',
+            '',
+            true,
+            true,
+            false,
+            Cookie::SAMESITE_STRICT
+        ))->json([
             'version' => 1.0
-        ];
+        ]);
     }
 }
