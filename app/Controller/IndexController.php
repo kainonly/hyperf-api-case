9c22f5d9e40dbd4f8cb7b33c73f6bdc5fb8ede7e
kain <zhangtqx@vip.qq.com>
2020-02-06 23:44:01 +0800

    测试Cookie

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -3,10 +3,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\Extra\Utils\UtilsInterface;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+
 class IndexController
 {
     public function index()
     {
         return ['version' => 1.0];
+    }
+
+    public function cookie(UtilsInterface $utils, ResponseInterface $response)
+    {
+        return $response->withCookie($utils->cookie('test', 'tester', [
+            'expire' => 0,
+            'path' => '/',
+            'domain' => '',
+            'secure' => true,
+            'httponly' => true,
+            'raw' => true,
+            'samesite' => 'strict',
+        ]));
     }
 }
