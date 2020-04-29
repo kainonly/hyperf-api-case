aa0c1d0d8859a7f49f03d7a0be50836934ca99df
kain <zhangtqx@vip.qq.com>
2020-04-29 15:11:20 +0800

    更新AMQPClient

--- config/autoload/amqp.php
+++ config/autoload/amqp.php
@@ -1,14 +1,25 @@
 <?php
+declare(strict_types=1);
+
+use Simplify\AMQP\AMQPClient;
+
+$ssl = (bool)env('AMQP_SSL', false);
+$sslContext = !$ssl ? null : AMQPClient::SSLContext([
+    'cafile' => '',
+    'verify_peer' => false
+]);
+$sslProtocol = !$ssl ? null : 'ssl';
+
 return [
     'clients' => [
         'default'
     ],
     'default' => [
         'host' => env('AMQP_HOST', 'localhost'),
-        'port' => 5672,
+        'port' => (int)env('AMQP_PORT', 5672),
         'user' => env('AMQP_USER', 'guest'),
-        'password' => env('AMQP_PASSWORD', 'guest'),
-        'vhost' => '/',
+        'password' => env('AMQP_PASS', 'guest'),
+        'vhost' => env('AMQP_VHOST', '/'),
         'concurrent' => [
             'limit' => 1,
         ],
@@ -26,9 +37,11 @@ return [
             'locale' => 'en_US',
             'connection_timeout' => 5.0,
             'read_write_timeout' => 5.0,
-            'context' => null,
+            'context' => $sslContext,
             'keepalive' => false,
-            'heartbeat' => 0
+            'heartbeat' => 0,
+            'channel_rpc_timeout' => 0.0,
+            'ssl_protocol' => $sslProtocol
         ],
     ],
 ];
