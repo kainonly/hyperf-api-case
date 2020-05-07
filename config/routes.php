1e457c7b564c1876656bed63b69e3c1c4f35c84e
kain <zhangtqx@vip.qq.com>
2020-05-07 11:14:01 +0800

    更新

--- config/routes.php
+++ config/routes.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 
 Router::get('/', [App\Controller\IndexController::class, 'index']);
+Router::post('/', [App\Controller\IndexController::class, 'index']);
 Router::addGroup('/system', function () {
     AutoController(App\Controller\System\MainController::class, [
         'middleware' => [
