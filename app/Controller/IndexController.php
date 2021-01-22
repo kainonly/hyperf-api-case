cc1be620d43d933e51c4ee6a29aac918738f65bc
kain <zhangtqx@vip.qq.com>
2021-01-22 20:16:44 +0800

    auto deploy

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -8,7 +8,7 @@ class IndexController
     public function index(): array
     {
         return [
-            'version' => 1.4,
+            'version' => 1.5,
             'unixtime' => time()
         ];
     }
