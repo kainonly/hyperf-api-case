b3a4cbe0fcfff51ddd5a43645c406a825a541106
kain <zhangtqx@vip.qq.com>
2021-04-23 17:24:04 +0800

    update

--- config/autoload/token.php
+++ config/autoload/token.php
@@ -1,14 +1,9 @@
 <?php
 declare(strict_types=1);
 return [
-    'default' => [
-        'issuer' => 'api.kainonly.com',
-        'audience' => '*',
-        'expires' => 900
-    ],
     'system' => [
         'issuer' => 'api.kainonly.com',
         'audience' => 'console.kainonly.com',
-        'expires' => 300
+        'expires' => 900
     ],
 ];
